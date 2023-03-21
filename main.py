d4b1ae67ce80134a056d1c239b2690130605ab25
Abhinandan Vyas <abhinandanvyas@Abhinandans-Air.domain.name>
2023-03-22 03:32:02 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Registering a rider
+# Registering a driver
+# Booking a ride within the given radius
+# Showing the ride history of the rider
+# Updating the location of the cab
+# Using In-memory database
+
+
+__init__(){
+    
+}
